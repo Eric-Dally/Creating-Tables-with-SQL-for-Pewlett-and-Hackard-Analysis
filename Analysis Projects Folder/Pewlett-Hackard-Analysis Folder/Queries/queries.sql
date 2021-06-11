fa9e2b031875e7715d0fd3ff6181715384c2ac0b
Eric Dally <82424250+Eric-Dally@users.noreply.github.com>
2021-06-11 13:45:33 -0500

    retirementdata

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/queries.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/queries.sql
@@ -1,1 +1,54 @@
-queries.sql
+queries.
+
+CREATE TABLE dept_emp (
+  dept_no VARCHAR(4) NOT NULL,
+  emp_no INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (dept_no, emp_no)
+);
+
+SELECT * FROM dept_emp;
+
+CREATE TABLE titles(
+	emp_no INT NOT NULL,
+	title VARCHAR (40) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY(emp_no, title, from_date)
+);
+
+SELECT * FROM titles;
+
+SELECT * FROM employees;
+
+SELECT first_name, last_name FROM employees
+WHERE birth_date BETWEEN '1/1/52' AND '12/31/55';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1/1/52' AND '12/31/52';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1/1/52' AND '12/31/55')
+AND (hire_date BETWEEN '1/1/85' AND '12/31/88');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1/1/52' AND '12/31/55')
+AND (hire_date BETWEEN '1/1/85' AND '12/31/88');
+
+-- Create new table
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1/1/52' AND '12/31/55')
+AND (hire_date BETWEEN '1/1/85' AND '12/31/88');
+
+SELECT * FROM retirement_info;
